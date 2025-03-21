031c57cfadb7ebe4c7ec60adeeb18237d40227e7
竹内結哉 <yuuya@MacBook-Pro.local>
2025-03-21 20:45:59 +0900

    初期設定完了

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzEAgBGBxjRg6teNwngi1pcjMqtujkuow",
+  authDomain: "reservation-app-d4ab1.firebaseapp.com",
+  projectId: "reservation-app-d4ab1",
+  storageBucket: "reservation-app-d4ab1.firebasestorage.app",
+  messagingSenderId: "18542921909",
+  appId: "1:18542921909:web:a4a0aab162f964df75d6fc",
+  measurementId: "G-NRLF9EHWJ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
